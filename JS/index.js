955856c97ebb109690806bebeeea6166523a507b
Paul Melton <melton.paul@hotmail.com>
2022-04-04 18:12:39 -0500

    Added modal with beginning of contact details

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+const openModal = document.getElementById("open-modal");
+const overlay = document.getElementById("overlay");
+const closeModal = document.getElementById("close-modal");
+const modal = document.getElementById("modal")
+
+openModal.addEventListener("click", function(){
+    overlay.style.display = "block"
+
+});
+closeModal.addEventListener("click", function(){
+    overlay.style.display = "none"
+
+});
+overlay.addEventListener("click", function(){
+    overlay.style.display = "none"
+
+});
