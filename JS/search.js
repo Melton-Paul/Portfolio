3aafc10ebdb9028349b8c82b8bf5fcd93bf3571c
Paul Melton <melton.paul@hotmail.com>
2022-04-05 18:09:57 -0500

    More cosmetic changes, as well as a fully functioning local search

--- JS/search.js
+++ JS/search.js
@@ -1,5 +1,15 @@
 const searchInput = document.getElementById("searchInput");
+
+
 searchInput.addEventListener("keyup", function(event){
     let search = event.target.value.toLowerCase();
-    console.log(search);
+    let allProjects = document.getElementsByClassName("project")
+    for (let counter = 0; counter < allProjects.length; counter++){
+        const currentItem = allProjects[counter].textContent.toLowerCase()
+        if (currentItem.includes(search)) {
+            allProjects[counter].style.display = "block"
+        } else {
+            allProjects[counter].style.display = "none"
+        }
+    }
 });
