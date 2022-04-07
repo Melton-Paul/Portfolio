2db408d35e54ac620a77964675ac3ed234647c14
Paul Melton <melton.paul@hotmail.com>
2022-04-07 17:19:37 -0500

    Refactored Dice Game

--- JS/search.js
+++ JS/search.js
@@ -26,33 +26,33 @@ const player1Dice = document.getElementById("player1Dice");
 const player2Dice = document.getElementById("player2Dice");
 const rollBtn = document.getElementById("rollBtn");
 const resetBtn = document.getElementById("resetBtn");
-hasGameFinished = false;
 
 
 let player1 = 0;
 let player2  = 0;
 let player1Turn = true;
+let hasGameFinished = false;
 
 rollBtn.addEventListener("click", function(){
     if (hasGameFinished === false){
         if (player1Turn){
-        let playerDice = randomNum();
-        player1 += playerDice;
-        player1Dice.textContent = playerDice;
-        player2Dice.classList.remove("active-player");
-        player1Dice.classList.add("active-player");
-        player1Scoreboard.textContent = player1;
-        player1Turn = false;
-    } else {
-        let playerDice = randomNum();
-        player2 += playerDice;
-        player1Dice.classList.remove("active-player");
-        player2Dice.classList.add("active-player");
-        player2Dice.textContent = playerDice;
-        player2Scoreboard.textContent = player2;
-        player1Turn = true;
-    }
-    gameFinished() };
+            let playerDice = randomNum();
+            player1 += playerDice;
+            player1Dice.textContent = playerDice;
+            player2Dice.classList.remove("active-player");
+            player1Dice.classList.add("active-player");
+            player1Scoreboard.textContent = player1;
+            player1Turn = false;} 
+        else {
+            let playerDice = randomNum();
+            player2 += playerDice;
+            player1Dice.classList.remove("active-player");
+            player2Dice.classList.add("active-player");
+            player2Dice.textContent = playerDice;
+            player2Scoreboard.textContent = player2;
+            player1Turn = true;}
+    gameFinished() 
+    };
 });
 resetBtn.addEventListener("click", resetGame);
 
@@ -63,16 +63,16 @@ function randomNum() {
 
 
 function gameFinished() {
-    if (player1 >= 20){
-        turnMessage.textContent = `Player 1 has Won the Game!`;
+    if (player1 >= 20 || player2 >= 20){
+        
+        hasGameFinished = true;
         resetBtn.style.display = "block";
         rollBtn.style.display = "none";
-        hasGameFinished = true;
-    } if (player2 >= 20){
-        turnMessage.textContent = `Player 2 has Won the Game!`;
-        resetBtn.style.display = "block";
-        rollBtn.style.display = "none";
-        hasGameFinished = true;
+
+        if (player1 >= 20){
+         turnMessage.textContent = `Player 1 has Won the Game!`}
+        if (player2 >= 20){
+         turnMessage.textContent = `Player 2 has Won the Game!`}
     };
 };
 function resetGame() {
