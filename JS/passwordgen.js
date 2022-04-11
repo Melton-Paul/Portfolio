e03bb31c9a5453d35aef5c86c0e1703a3dfb6321
Paul Melton <melton.paul@hotmail.com>
2022-04-11 18:55:46 -0500

    Reoptimized password generator

--- JS/passwordgen.js
+++ JS/passwordgen.js
@@ -7,53 +7,35 @@ let password2 = document.getElementById("password2")
 let password3 = document.getElementById("password3")
 let password4 = document.getElementById("password4")
 
-
-// function ranChar() {
+// function password(){
 //     let password = ""
-//     for (let i = 0; i < 12; i++) {
-//         let random = Math.floor(Math.random() * randomChar.length)
-//         password = randomChar[random]
+//     for (let i = 0; i < passwordInput.value; i++){
+//         password += randomChar[Math.floor(Math.random() * randomChar.length)]
 //     }
-//     console.log(password)
-    
+//     return password
+// }
+
+// function generatePass() {
+//     password1.textContent= password()
+//     password2.textContent= password()
+//     password3.textContent= password()
+//     password4.textContent= password()
 // }
 
 
-function password(){
-    let password = ""
-    for (let i = 0; i < passwordInput.value; i++){
-        password += randomChar[Math.floor(Math.random() * randomChar.length)]
-    }
-    return password
+
+
+function generatePasswords() {
+    password1.textContent = passGenerator()
+    password2.textContent = passGenerator()
+    password3.textContent = passGenerator()
+    password4.textContent = passGenerator()
 }
 
-function generatePass() {
-    password1.textContent= password()
-    password2.textContent= password()
-    password3.textContent= password()
-    password4.textContent= password()
+function passGenerator() {
+    const ranPossiblities = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0,"!","?"]
+    return new Array(12).fill(0).map(function(){
+        return ranPossiblities[Math.floor(Math.random()* ranPossiblities.length)]
+    }).join(``)
+
 }
-
-
-
-
-// passwordInput.addEventListener("oninput", function(){
-//     console.log("it works")
-// })
-
-
-// function generatePasswords() {
-//     password1.textContent = passGenerator()
-//     password2.textContent = passGenerator()
-//     password3.textContent = passGenerator()
-//     password4.textContent = passGenerator()
-// }
-
-// function passGenerator() {
-//     const ranPossiblities = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0,"!","?"]
-//     const passLength = passwordInput.value
-//     return new Array(passLength).fill(0).map(function(){
-//         return ranPossiblities[Math.floor(Math.random()* ranPossiblities.length)]
-//     }).join(``)
-
-// }
