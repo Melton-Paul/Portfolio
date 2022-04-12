a8593764ffb6d4b7aabd2621fdd3f83bea6f6126
Paul Melton <melton.paul@hotmail.com>
2022-04-12 15:41:39 -0500

    Fixed password generator using unoptimized version

--- JS/passwordgen.js
+++ JS/passwordgen.js
@@ -1,37 +1,41 @@
 const passwordInput = document.getElementById("passwordInput")
+const generatePasswords = document.getElementById("generatePasswords")
 
 let password1 = document.getElementById("password1")
 let password2 = document.getElementById("password2")
 let password3 = document.getElementById("password3")
 let password4 = document.getElementById("password4")
-
-// function password(){
-//     let password = ""
-//     for (let i = 0; i < passwordInput.value; i++){
-//         password += randomChar[Math.floor(Math.random() * randomChar.length)]
-//     }
-//     return password
-// }
-
-// function generatePass() {
-//     password1.textContent= password()
-//     password2.textContent= password()
-//     password3.textContent= password()
-//     password4.textContent= password()
-// }
+const ranPossiblities = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0,"!","?"]
 
 
+generatePasswords.addEventListener("click", generatePass)
 
-function generatePasswords() {
-    password1.textContent = passGenerator()
-    password2.textContent = passGenerator()
-    password3.textContent = passGenerator()
-    password4.textContent = passGenerator()
+function password(){
+    let password = ""
+    for (let i = 0; i < passwordInput.value; i++){
+        password += ranPossiblities[Math.floor(Math.random() * ranPossiblities.length)]
+    }
+    return password
 }
 
-function passGenerator(num) {
-    const ranPossiblities = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0,"!","?"]
-    return new Array(num).fill(0).map(function(){
-        return ranPossiblities[Math.floor(Math.random()* ranPossiblities.length)]
-    }).join(``)
+function generatePass() {
+    password1.textContent= password()
+    password2.textContent= password()
+    password3.textContent= password()
+    password4.textContent= password()
 }
+
+
+
+// function generatePasswords() {
+//     password1.textContent = passGenerator()
+//     password2.textContent = passGenerator()
+//     password3.textContent = passGenerator()
+//     password4.textContent = passGenerator()
+// }
+
+// function passGenerator(num) {
+//     return new Array(num).fill(0).map(function(){
+//         return ranPossiblities[Math.floor(Math.random()* ranPossiblities.length)]
+//     }).join(``)
+// }
